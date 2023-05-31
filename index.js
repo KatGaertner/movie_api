84bc948f194f440b69c499830913b8ee5bdfe1d6
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-05-31 12:11:20 +0200

    implemented CORS

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) {
+            return callback(null, true);
+        }
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let message = 'The CORS policy for this application doesn\'t allow acces from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
