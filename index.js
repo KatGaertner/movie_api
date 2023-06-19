efae05ce5c23f99bf2fcd394c49241a9e0e91ad5
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-06-19 12:21:53 +0200

    connected to online DB

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://127.0.0.1:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://127.0.0.1:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
