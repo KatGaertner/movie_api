6b7807882f89310dc358f6b3b108e3c7ae79130f
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-06-21 11:26:21 +0200

    added cors back in

--- index.js
+++ index.js
@@ -25,21 +25,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-// const allowedOrigins = ['http://localhost:8080', 'http://127.0.0.1:1234'];
+const allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
 app.use(cors(
-    // {
-    //     origin: (origin, callback) => {
-    //         if (!origin) {
-    //             return callback(null, true);
-    //         }
-    //         if (allowedOrigins.indexOf(origin) === -1) {
-    //             let message = 'The CORS policy for this application doesn\'t allow acces from origin ' + origin;
-    //             return callback(new Error(message), false);
-    //         }
-    //         return callback(null, true);
-    //     }
-    // }
+    {
+        origin: (origin, callback) => {
+            if (!origin) {
+                return callback(null, true);
+            }
+            if (allowedOrigins.indexOf(origin) === -1) {
+                let message = 'The CORS policy for this application doesn\'t allow acces from origin ' + origin;
+                return callback(new Error(message), false);
+            }
+            return callback(null, true);
+        }
+    }
 ));
 
 let auth = require('./auth')(app);
