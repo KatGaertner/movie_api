4fc3788ecb5654f2d8bc8510520ab794ac5f61f0
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-08-29 21:44:43 +0200

    added my github to allowed origins

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 const allowedOrigins = [
-    'http://localhost:8080', 'http://localhost:1234', 'https://myflix-27.netlify.app', 'http://localhost:4200'
+    'http://localhost:8080', 'http://localhost:1234', 'https://myflix-27.netlify.app', 'http://localhost:4200', 'https://katgaertner.github.io'
 ];
 
 app.use(cors(
